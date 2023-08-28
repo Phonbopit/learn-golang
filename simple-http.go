d40c3e1b9bc8b75982bca3ceecd824dd52286d01
Chai Phonbopit <phonbopit@gmail.com>
2023-08-28 22:07:52 +0700

    add more sample http handle with json

--- simple-http.go
+++ simple-http.go
@@ -1,14 +1,49 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
-  http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello World!")
-  })
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World!")
+	})
 
-  http.ListenAndServe(":7777", nil)
+	http.HandleFunc("/json", handleJson)
+	http.HandleFunc("/no-access", handleNoAccess)
+
+	http.ListenAndServe(":7777", nil)
+}
+
+func handleJson(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	res := make(map[string]string)
+	res["message"] = "Hello World!"
+
+	jsonResponse, err := json.Marshal(res)
+	if err != nil {
+		log.Fatalf("Error in JSON format. Err : %s", err)
+	}
+
+	w.Write(jsonResponse)
+	return
+}
+
+func handleNoAccess(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusUnauthorized)
+	w.Header().Set("Content-Type", "application/json")
+
+	res := make(map[string]string)
+	res["message"] = "Unauthorized"
+	res["code"] = "10000"
+
+	jsonResponse, err := json.Marshal(res)
+	if err != nil {
+		log.Fatalf("Error in JSON. Err : %s", err)
+	}
+
+	w.Write(jsonResponse)
 }
